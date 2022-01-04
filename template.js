53234cd55020c417ee6c37c3c9eee1d6019f9b40
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2022-01-04 17:14:12 +0000

    Add template

---
+++ template.js
@@ -0,0 +1,16 @@
+function foo(data) {
+}
+
+
+const sample = ``;
+const expectation = 0;
+const test = foo(sample);
+console.assert(test === expectation, `\n\tExpected: ${expectation}\n\tGot: ${test}`);
+
+const input = ``;
+
+// console.log(foo(input));
+
+
+
+console.log(input);
